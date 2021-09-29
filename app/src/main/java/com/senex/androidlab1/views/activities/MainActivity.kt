8805b731afc93a1e503010be8a60e3cae8f91bcb
V-Senex <junior.07.inator@gmail.com>
2021-09-29 22:14:24 +0300

    Dependencies cleanup

--- app/src/main/java/com/senex/androidlab1/views/activities/MainActivity.kt
+++ app/src/main/java/com/senex/androidlab1/views/activities/MainActivity.kt
@@ -1,16 +1,13 @@
 package com.senex.androidlab1.views.activities
 
-import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.app.AppCompatDelegate
 import com.senex.androidlab1.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
 
     }
 }
