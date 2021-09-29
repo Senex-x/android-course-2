39f968596039785328ae7b3e3335e8b13f0619a4
V-Senex <junior.07.inator@gmail.com>
2021-09-29 21:21:03 +0300

    Cleanup done

--- app/src/main/java/com/senex/androidlab1/views/activities/MainActivity.kt
+++ app/src/main/java/com/senex/androidlab1/views/activities/MainActivity.kt
@@ -1,22 +1,13 @@
 package com.senex.androidlab1.views.activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.senex.androidlab1.R
-import com.senex.androidlab1.examples.ExampleChildBarImpl
-import com.senex.androidlab1.examples.ExampleChildFooImpl
-import com.senex.androidlab1.utils.log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val e1 = ExampleChildFooImpl("data", "optional")
-        val e2 = ExampleChildBarImpl("data")
-
-        e1.foo("string").toString().log()
-        e2.bar("string").log()
     }
 }
 
