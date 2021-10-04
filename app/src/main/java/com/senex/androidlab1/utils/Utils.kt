1b93a7464820d12c42a3b6e310742b8e1f7abdf6
V-Senex <junior.07.inator@gmail.com>
2021-10-04 19:03:45 +0300

    Core files update

--- app/src/main/java/com/senex/androidlab1/utils/Utils.kt
+++ app/src/main/java/com/senex/androidlab1/utils/Utils.kt
@@ -1,10 +1,27 @@
 package com.senex.androidlab1.utils
 
+import android.content.Context
 import android.util.Log
+import android.widget.Toast
+
+internal fun Context.toast(message: String?) =
+    message?.let {
+        Toast.makeText(this, it, Toast.LENGTH_SHORT).show()
+    } ?: Unit
+
+internal fun Context.toast(messageId: Int?) =
+    messageId?.let {
+        toast(this.getString(it))
+    } ?: Unit
+
+internal fun Int?.toast(context: Context) =
+    context.toast(this)
+
+internal fun String?.toast(context: Context) =
+    context.toast(this)
 
 internal fun log(message: String?) =
-    Log.d("app-debug", message ?: "null")
+    Log.d("App-debug", message ?: "null")
 
-@JvmName("logExt")
-internal fun String?.log() =
-    log(this)
+private fun String.present() =
+    isNotEmpty() && isNotBlank()
