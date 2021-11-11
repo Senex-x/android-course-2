62f42cd53dd5b7bbbacb3a11d26aee1294e9abfc
V-Senex <junior.07.inator@gmail.com>
2021-11-11 16:27:51 +0300

    Add basic `InteractiveListFragment`

---
+++ app/src/main/java/com/senex/androidlab1/views/fragments/InteractiveListFragment.kt
@@ -0,0 +1,54 @@
+package com.senex.androidlab1.views.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.senex.androidlab1.adapters.UserRecyclerAdapter
+import com.senex.androidlab1.database.AppDatabaseMain
+import com.senex.androidlab1.databinding.FragmentInteractiveListBinding
+import com.senex.androidlab1.databinding.FragmentListBinding
+import com.senex.androidlab1.utils.MarginItemDecoration
+
+
+class InteractiveListFragment : Fragment() {
+    private var _binding: FragmentInteractiveListBinding? = null
+    private val binding
+        get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentInteractiveListBinding.inflate(inflater, container, false)
+
+        binding.recyclerMain.init()
+
+        return binding.root
+    }
+
+    private fun RecyclerView.init() {
+        layoutManager = LinearLayoutManager(
+            requireContext()
+        )
+
+        adapter = UserRecyclerAdapter(
+            AppDatabaseMain.database.userDao().getAll()
+        ) {
+            // Setup onclick listener
+        }
+
+        addItemDecoration(
+            MarginItemDecoration(20)
+        )
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
