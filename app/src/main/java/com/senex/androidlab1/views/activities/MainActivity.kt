46a7f588c72a41853abb99e0113b3fc20760b3b0
V-Senex <junior.07.inator@gmail.com>
2021-11-11 16:37:08 +0300

    Add bottom navigation view use

--- app/src/main/java/com/senex/androidlab1/views/activities/MainActivity.kt
+++ app/src/main/java/com/senex/androidlab1/views/activities/MainActivity.kt
@@ -2,8 +2,12 @@ package com.senex.androidlab1.views.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.senex.androidlab1.R
 import com.senex.androidlab1.database.AppDatabaseMain
+import com.senex.androidlab1.databinding.ActivityMainBinding
 import com.senex.androidlab1.models.User
 import com.senex.androidlab1.utils.generateUsers
 import com.senex.androidlab1.utils.log
@@ -12,17 +16,27 @@ import java.util.*
 import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         // Singleton initialization
         AppDatabaseMain.init(this)
 
+        // Setting up navigation host for Jetpack's Navigation component framework
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment_main) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        // Setting up navigation view for Jetpack's Navigation component framework
+        binding.bottomNavigationViewMain.setupWithNavController(navController)
+
+        // Debugging block
         val userDao = AppDatabaseMain.database.userDao()
-
         //userDao.deleteAll()
-
         userDao.insertAll(
             //*generateUsers(20).toTypedArray()
         )
