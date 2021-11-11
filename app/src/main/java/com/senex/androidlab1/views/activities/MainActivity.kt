5b463333f5dc2685743e8e8d5923252397238122
V-Senex <junior.07.inator@gmail.com>
2021-11-11 15:38:22 +0300

    Fix package mismatch

--- app/src/main/java/com/senex/androidlab1/views/activities/MainActivity.kt
+++ app/src/main/java/com/senex/androidlab1/views/activities/MainActivity.kt
@@ -1,15 +1,9 @@
-package com.senex.java.androidlab1.views.activities
+package com.senex.androidlab1.views.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.senex.androidlab1.R
 import com.senex.androidlab1.database.AppDatabaseMain
-import com.senex.androidlab1.models.User
-import com.senex.androidlab1.utils.generateUsers
-import com.senex.androidlab1.utils.log
-import io.github.serpro69.kfaker.faker
-import java.util.*
-import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
