6b8fbe9116ab6ee7edc93918fd9e4895650b37e4
V-Senex <junior.07.inator@gmail.com>
2021-11-18 23:17:31 +0300

    Add base activities

--- app/src/main/java/com/senex/androidlab1/views/activities/WakeActivity.kt
+++ app/src/main/java/com/senex/androidlab1/views/activities/WakeActivity.kt
@@ -2,17 +2,16 @@ package com.senex.androidlab1.views.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.senex.androidlab1.databinding.ActivityMainBinding
+import com.senex.androidlab1.databinding.ActivityWakeBinding
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+class WakeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityWakeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityWakeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
 
     }
 }
-
