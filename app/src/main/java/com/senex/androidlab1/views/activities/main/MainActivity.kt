4fd3f89ca148d109f470ec0faa168ef86868f273
V-Senex <junior.07.inator@gmail.com>
2021-11-21 16:28:01 +0300

    Refactor `MainActivity` `TimePicker` handling

--- app/src/main/java/com/senex/androidlab1/views/activities/main/MainActivity.kt
+++ app/src/main/java/com/senex/androidlab1/views/activities/main/MainActivity.kt
@@ -1,54 +1,38 @@
 package com.senex.androidlab1.views.activities.main
 
-
+import android.app.Activity
+import android.database.ContentObserver
 import android.media.AudioManager
 import android.media.MediaPlayer
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.provider.Settings.System.CONTENT_URI
+import android.widget.TimePicker
 import androidx.appcompat.app.AppCompatActivity
 import com.senex.androidlab1.R
 import com.senex.androidlab1.databinding.ActivityMainBinding
 import com.senex.androidlab1.views.activities.main.observers.VolumeObserver
 import java.util.*
 
-
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private lateinit var volumeObserver: VolumeObserver
 
+    private lateinit var volumeObserver: VolumeObserver
+    private lateinit var mediaPlayer: MediaPlayer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        volumeObserver = VolumeObserver(
-            context = this,
-            Handler(Looper.getMainLooper())
-        )
+        // These two are not available before onCreate()
+        volumeObserver = configureVolumeObserver()
+        mediaPlayer = configureMediaPlayer()
 
-        contentResolver.registerContentObserver(
-            CONTENT_URI,
-            true,
-            volumeObserver
-        )
+        registerObserver(volumeObserver)
 
-        binding.timePicker.apply {
-            setIs24HourView(true)
-
-            val mediaPlayer = MediaPlayer.create(context, R.raw.time_picker_tick)
-            val audioManager = getSystemService(AUDIO_SERVICE) as AudioManager
-            mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC)
-            setOnTimeChangedListener { timePicker, i, i1 ->
-                val systemVolumeLevel = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC)
-                val volumeLevel: Float = calculateVolumeLevel(systemVolumeLevel)
-                mediaPlayer.setVolume(volumeLevel, volumeLevel) //set volume takes two parameters
-                mediaPlayer.seekTo(0)
-                mediaPlayer.start()
-            }
-        }
+        binding.timePicker.configure()
     }
 
     override fun onResume() {
@@ -65,8 +49,42 @@ class MainActivity : AppCompatActivity() {
     override fun onDestroy() {
         super.onDestroy()
 
-        contentResolver.unregisterContentObserver(volumeObserver)
+        unregisterObserver(volumeObserver)
     }
+
+    private fun TimePicker.configure() {
+        setIs24HourView(true)
+
+        setOnTimeChangedListener { _, _, _ ->
+            mediaPlayer.seekTo(0)
+            mediaPlayer.start()
+        }
+    }
+
+    private fun configureVolumeObserver() =
+        VolumeObserver(
+            context = this,
+            Handler(Looper.getMainLooper())
+        ) { systemVolumeLevel ->
+            val volumeLevel: Float = calculateVolumeLevel(systemVolumeLevel)
+            mediaPlayer.setVolume(volumeLevel, volumeLevel)
+        }
+
+    private fun Activity.registerObserver(observer: ContentObserver): Unit =
+        contentResolver.registerContentObserver(
+            CONTENT_URI,
+            true,
+            observer
+        )
+
+    private fun Activity.unregisterObserver(observer: ContentObserver): Unit =
+        contentResolver.unregisterContentObserver(observer)
+
+    private fun configureMediaPlayer() = MediaPlayer
+        .create(this, R.raw.time_picker_tick)
+        .apply {
+            setAudioStreamType(AudioManager.STREAM_MUSIC)
+        }
 
     private fun calculateVolumeLevel(systemVolumeLevel: Int): Float {
         val section = 1 / 14f
--- app/src/main/java/com/senex/androidlab1/views/activities/main/observers/VolumeObserver.kt
+++ app/src/main/java/com/senex/androidlab1/views/activities/main/observers/VolumeObserver.kt
@@ -4,12 +4,11 @@ import android.content.Context
 import android.database.ContentObserver
 import android.media.AudioManager
 import android.os.Handler
-import com.senex.androidlab1.utils.log
-
 
 class VolumeObserver(
     context: Context,
-    handler: Handler
+    handler: Handler,
+    val onVolumeChangeListener: (newVolumeLevel: Int) -> Unit
 ) : ContentObserver(handler) {
 
     private val audioManager = context
@@ -17,7 +16,7 @@ class VolumeObserver(
 
     override fun onChange(selfChange: Boolean) {
         val currentVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC)
-        log("Volume now is $currentVolume")
+        onVolumeChangeListener(currentVolume)
     }
 
     override fun deliverSelfNotifications() = false
