c4270862e63972515fff2a95cf3026c2fbc29497
V-Senex <junior.07.inator@gmail.com>
2021-11-22 22:52:03 +0300

    Add `BootReceiver`

---
+++ app/src/main/java/com/senex/androidlab1/views/activities/main/notifications/receivers/BootReceiver.kt
@@ -0,0 +1,13 @@
+package com.senex.androidlab1.views.activities.main.notifications.receivers
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import com.senex.androidlab1.utils.log
+
+class BootReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        log("RECEIVED")
+        println("RECEIVED>?????")
+    }
+}
