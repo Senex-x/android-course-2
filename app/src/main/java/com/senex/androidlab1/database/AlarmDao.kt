8371d604e97487009e7a762241f18bdb728d4a36
V-Senex <junior.07.inator@gmail.com>
2021-11-23 23:14:37 +0300

    Add `Room` implementation to store `Alarm` instances

---
+++ app/src/main/java/com/senex/androidlab1/database/AlarmDao.kt
@@ -0,0 +1,28 @@
+package com.senex.androidlab1.database
+
+import androidx.room.*
+import com.senex.androidlab1.model.Alarm
+
+@Dao
+interface AlarmDao {
+    @Insert
+    fun insert(alarm: Alarm)
+
+    @Query("SELECT * FROM alarms WHERE notificationId = :id")
+    fun get(id: Int): Alarm
+
+    @Query("SELECT * FROM alarms")
+    fun getAll(): List<Alarm>
+
+    @Update
+    fun update(alarm: Alarm)
+
+    @Delete
+    fun delete(alarm: Alarm)
+
+    @Query("DELETE FROM alarms WHERE notificationId = :id")
+    fun deleteByKey(id: Int)
+
+    @Query("DELETE FROM alarms")
+    fun deleteAll()
+}
