fb3054f3dee61a2fdcf94050097f8be31f6488a8
V-Senex <junior.07.inator@gmail.com>
2021-11-26 13:17:22 +0300

    Handle incorrect `Track` id pass

--- app/src/main/java/com/senex/androidlab1/views/fragments/info/MusicInfoFragment.kt
+++ app/src/main/java/com/senex/androidlab1/views/fragments/info/MusicInfoFragment.kt
@@ -5,12 +5,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import com.senex.androidlab1.databinding.FragmentMusicInfoBinding
+import com.senex.androidlab1.models.Track
+import com.senex.androidlab1.repository.TrackRepository
+import com.senex.androidlab1.utils.toast
 
 class MusicInfoFragment : Fragment() {
+    private val args: MusicInfoFragmentArgs by navArgs()
     private var _binding: FragmentMusicInfoBinding? = null
     private val binding
         get() = _binding!!
+
+    private lateinit var track: Track
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -19,7 +27,21 @@ class MusicInfoFragment : Fragment() {
     ): View {
         _binding = FragmentMusicInfoBinding.inflate(inflater, container, false)
 
+        val nullableTrack = TrackRepository.getByPrimaryKey(args.trackId)
+
+        if (nullableTrack == null) {
+            requireContext().toast("Unknown track identifier")
+            findNavController().popBackStack()
+        } else {
+            track = nullableTrack
+        }
+
+        initTextViews()
+
         return binding.root
+    }
+
+    private fun initTextViews() {
     }
 
     override fun onDestroyView() {
