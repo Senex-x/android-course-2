a1f50d0afe5631e16c85e9d82755c2dddcbbea5d
V-Senex <junior.07.inator@gmail.com>
2021-12-21 17:56:05 +0300

    Add `Toolbar` options menu inflating

--- app/src/main/java/com/senex/androidlab1/views/activities/main/MainActivity.kt
+++ app/src/main/java/com/senex/androidlab1/views/activities/main/MainActivity.kt
@@ -1,19 +1,22 @@
 package com.senex.androidlab1.views.activities.main
 
 import android.os.Bundle
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
 import com.senex.androidlab1.R
 import com.senex.androidlab1.database.AppDatabaseMain
 import com.senex.androidlab1.models.Note
 import com.senex.androidlab1.repositories.NoteRepository
 import com.senex.androidlab1.utils.log
-
 import java.util.*
 
+
+// TODO: add binding to dev
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.main_toolbar))
 
         AppDatabaseMain.init(applicationContext)
 
@@ -30,6 +33,11 @@ class MainActivity : AppCompatActivity() {
         log("Database snapshot: " +
                 AppDatabaseMain.database.noteDao().getAll().toString()
         )
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_main, menu)
+        return true
     }
 }
 
